3655220979be3c3660ff980ea855a193c3b2c5ff
lubanatest <32592478+lubanatest@users.noreply.github.com>
2018-10-06 12:12:15 +1000

    Update users.js

--- users.js
+++ users.js
@@ -56,3 +56,23 @@ function setUsers(fileName, callback)
 		reader.readAsText(fileUpload.files[0]);
 	return users;
 };
+
+function createTable(tableData) {
+  var table = document.createElement('table');
+  var tableBody = document.createElement('tbody');
+
+  tableData.forEach(function(rowData) {
+    var row = document.createElement('tr');
+
+    rowData.forEach(function(cellData) {
+      var cell = document.createElement('td');
+      cell.appendChild(document.createTextNode(cellData.name));
+      row.appendChild(cell);
+    });
+
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+}
